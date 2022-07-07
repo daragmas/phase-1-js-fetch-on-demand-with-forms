9ffce49aa920e5a299d4554f92e20732b6cb2cfa
daragmas <daragmas@gmail.com>
2022-07-06 22:11:18 -0400

    commit tree 5c442f5e7409eeea0221ebff244cc32969557209

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
 const init = () => {
+    const inputForm = document.querySelector('form')
+    inputForm.addEventListener('submit',(e)=>{
+        e.preventDefault()
+        const input = document.querySelector('input#searchByID');
+
+        fetch('http://localhost:3000/movies')
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+            })
+    })
   
 }
 
